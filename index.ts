03b3092a1f027bd5d845aaf172934c7ad945f3b3
Hannes Rüger <hannes@rueger.events>
2024-10-22 15:17:38 +0200

    chore: reformat

--- index.ts
+++ index.ts
@@ -3,109 +3,109 @@ import { Prisma, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 function testTypePayloadForPost(
-  post: Prisma.PostGetPayload<{
-    include: {
-      user: {
+    post: Prisma.PostGetPayload<{
         include: {
-          profile: {
-            select: {
-              description: true;
+            user: {
+                include: {
+                    profile: {
+                        select: {
+                            description: true;
+                        };
+                    };
+                };
             };
-          };
         };
-      };
-    };
-  }>
+    }>
 ) {
-  if (post.user.profile !== null) {
-    console.log(post.user.profile.description);
-  }
+    if (post.user.profile !== null) {
+        console.log(post.user.profile.description);
+    }
 }
 
 function testTypePayloadForUser(
-  user: Prisma.UserGetPayload<{
-    include: {
-      profile: {
-        select: {
-          description: true;
+    user: Prisma.UserGetPayload<{
+        include: {
+            profile: {
+                select: {
+                    description: true;
+                };
+            };
         };
-      };
-    };
-  }>
+    }>
 ) {
-  if (user.profile !== null) {
-    console.log(user.profile.description);
-  }
+    if (user.profile !== null) {
+        console.log(user.profile.description);
+    }
 }
 
 async function main() {
-  // Clear db
-  await prisma.user.deleteMany({});
-  await prisma.profile.deleteMany({});
-  await prisma.post.deleteMany({});
+    // Clear db
+    await prisma.user.deleteMany({});
+    await prisma.profile.deleteMany({});
+    await prisma.post.deleteMany({});
 
-  // Create test user
-  await prisma.user.create({
-    data: {
-      id: "1",
-      name: "first user",
-      profile: {
-        create: {
-          id: "1",
-          age: 22,
-          description: "hello world",
-        },
-      },
-      posts: {
-        create: [
-          {
+    // Create test user
+    await prisma.user.create({
+        data: {
             id: "1",
-            title: "my post",
-            content: "my content",
-          },
-        ],
-      },
-    },
-  });
-
-  const post = await prisma.post.findUnique({
-    where: {
-      id: "1",
-    },
-    include: {
-      user: {
-        include: {
-          profile: {
-            select: {
-              description: false,
-              age: true,
+            name: "first user",
+            profile: {
+                create: {
+                    id: "1",
+                    age: 22,
+                    description: "hello world",
+                },
             },
-          },
+            posts: {
+                create: [
+                    {
+                        id: "1",
+                        title: "my post",
+                        content: "my content",
+                    },
+                ],
+            },
         },
-      },
-    },
-  });
-  if (post === null) {
-    console.error("something went wrong");
-    return;
-  }
+    });
 
-  // NO TS LINTING ERROR
-  testTypePayloadForPost(post);
+    const post = await prisma.post.findUnique({
+        where: {
+            id: "1",
+        },
+        include: {
+            user: {
+                include: {
+                    profile: {
+                        select: {
+                            description: false,
+                            age: true,
+                        },
+                    },
+                },
+            },
+        },
+    });
+    if (post === null) {
+        console.error("something went wrong");
+        return;
+    }
 
-  // TS LINTING ERROR
-  // testTypePayloadForUser(post.user);
+    // NO TS LINTING ERROR - SHOULD BE
+    testTypePayloadForPost(post);
+
+    // TS LINTING ERROR - EXPECTED
+    testTypePayloadForUser(post.user);
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
+    .then(async () => {
+        await prisma.$disconnect();
+    })
 
-  .catch(async (e) => {
-    console.error(e);
+    .catch(async (e) => {
+        console.error(e);
 
-    await prisma.$disconnect();
+        await prisma.$disconnect();
 
-    process.exit(1);
-  });
+        process.exit(1);
+    });
